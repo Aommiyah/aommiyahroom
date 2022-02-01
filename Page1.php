03220d0667f666664dbb188ef340c5d50778ac3d
Aommiyah <98304413+Aommiyah@users.noreply.github.com>
2022-02-01 14:40:36 +0700

    Create Page1.php

---
+++ Page1.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>page 1</title>
+  </head>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+  <br>
+    <b>ชื่อ :</b> <i>นางสาวพชรภร หอมจันทร์</i>
+  <br>
+    <b>ชื่อเล่น : </b> <i>ออม</i>
+  <br>
+    <b>ชั้น : </b> <i>ม.4/12</i>
+  <br>
+    <b>เลขที่ :</b> <i>20</i>
+  <br>
+    <b>เลขประจำตัว :</b> <i>57066</i>
+  <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม</i>
+  </body>
+</html>
