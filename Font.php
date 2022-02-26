305e504826c84821a1c57c0c87fd6bdee16f609f
Aommiyah <98304413+Aommiyah@users.noreply.github.com>
2022-02-26 20:11:46 +0700

    Update Font.php

--- Font.php
+++ Font.php
@@ -8,7 +8,7 @@
       }
       @font-face{
         font-family: "MyFont2";
-        src: url("ChocolateteehaypaiTEST.ttf");
+        src: url("FCFlexica.ttf");
       }
       #font1{
         font-family: "MyFont1";
