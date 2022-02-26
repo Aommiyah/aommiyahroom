64ad5125930f4ab11132593806674fade7a59820
Aommiyah <98304413+Aommiyah@users.noreply.github.com>
2022-02-26 20:08:15 +0700

    Update Font.php

--- Font.php
+++ Font.php
@@ -4,7 +4,7 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("Doughut.ttf");
+        src: url("Doughnut.ttf");
       }
       @font-face{
         font-family: "MyFont2";
