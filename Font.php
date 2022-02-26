2a2f7f0366ffe0b6a1253277c19a0843124e12fc
Aommiyah <98304413+Aommiyah@users.noreply.github.com>
2022-02-26 19:01:23 +0700

    Create Font.php

---
+++ Font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("FC-Flexica.zip");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("Chocolateteehaypai.zip");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+      font-familym:  "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1> id="font1">นางสาวพชรภร หอมจันทร์ เลขที่ 20 ห้อง ม.4/12 เลขประจำตัว 57066</h1>
+    <h1> id="font2">นางสาวพชรภร หอมจันทร์ เลขที่ 20 ห้อง ม.4/12 เลขประจำตัว 57066</h1>
+  </body>
+</html>
